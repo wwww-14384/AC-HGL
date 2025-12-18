f0eb35285cae914a0a567ffd0684149097eddc0e
wy deng <kangna009814384@163.com>
2025-12-18 14:06:26 +0800

    Update base_model.py

--- base_model.py
+++ base_model.py
@@ -370,7 +370,7 @@ class SequenceModel():
 
             if train_loss <= self.train_stop_loss_thred:
                 break
-        torch.save(best_param, f'{self.save_path}{self.save_prefix}achgl_{self.seed}.pkl')
+        torch.save(best_param, f'{self.save_path}{self.save_prefix}AC-HGL_{self.seed}.pkl')
 
 
 
@@ -427,5 +427,6 @@ class SequenceModel():
         }
 
         return predictions, metrics
+
 
 
